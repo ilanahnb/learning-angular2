e79cdea4fd5d3c9b31cd58babde8169fd43d9895
Ilana Boeira <ilanaboeira@gmail.com>
2017-08-29 15:13:47 +1000

    updated images and data

--- app/component.app.ts
+++ app/component.app.ts
@@ -2,6 +2,14 @@
 
 import { Component } from '@angular/core';
 
+export class Pet {
+  name: string;
+  shortname: string;
+  age: string;
+  bio: string
+}
+
+
 // Component decorator
 @Component({
   selector: 'app',  // html tag
@@ -12,39 +20,42 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  name: string;
-  pets: any;
-
-  onClick(petItem, petElement) {
-    this.name = petItem.name;
-    petElement.style.backgroundColor="#FECE4E";
-  }
+  pets = PETS;
+  currentPet: Pet;
 
   addPet(value) {
     if (value!=='') {
       this.pets.push({
         name: value,
-        age: '_ years'
+        shortname: 'Pet_Avatar',
+        age: '',
+        bio: ''
       });
     }
   }
-
-  // method that gets executed when
-  // an instance of the class is created
-  constructor() {
-    this.pets = [
-      {
-        name: 'Panqueca',
-        age: '7 years'
-      },
-      {
-        name: 'Jello',
-        age: '2 years'
-      },
-      {
-        name: 'Quindim',
-        age: '3 years'
-      }
-    ]
-  }
 }
+
+
+var PETS: Pet[] = [
+  {
+    "name":"Panqueca",
+    "shortname":"Panqueca",
+    "age":"7 years",
+    "bio":"Barot has just finished his final year at The Royal Academy of Painting and Sculpture, where he excelled in glass etching paintings and portraiture. Hailed as one of the most diverse artists of his generation, Barot is equally as skilled with watercolors as he is with oils, and is just as well-balanced in different subject areas. Barot's collection entitled \"The Un-Collection\" will adorn the walls of Gilbert Hall, depicting his range of skills and sensibilities - all of them, uniquely Barot, yet undeniably different"
+  }, {
+    "name":"Jello",
+    "shortname":"Jello",
+    "age":"2 years",
+    "bio":"The Artist to Watch in 2012 by the London Review, Johnathan has already sold one of the highest priced-commissions paid to an art student, ever on record. The piece, entitled Gratitude Resort, a work in oil and mixed media, was sold for $750,000 and Jonathan donated all the proceeds to Art for Peace, an organization that provides college art scholarships for creative children in developing nations"
+  }, {
+    "name":"Quindim",
+    "shortname":"Quindim",
+    "age":"2 years",
+    "bio":"Hillary is a sophomore art sculpture student at New York University, and has already won all the major international prizes for new sculptors, including the Divinity Circle, the International Sculptor's Medal, and the Academy of Paris Award. Hillary's CAC exhibit features 25 abstract watercolor paintings that contain only water images including waves, deep sea, and river."
+  }, {
+    "name":"Doge",
+    "shortname":"Doge",
+    "age":"99 years",
+    "bio":"The Art College in New Dehli has sponsored Hassum on scholarship for his entire undergraduate career at the university, seeing great promise in his contemporary paintings of landscapes - that use equal parts muted and vibrant tones, and are almost a contradiction in art. Hassum will be speaking on \"The use and absence of color in modern art\" during Thursday's agenda."
+  }
+]
